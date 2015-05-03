69c93b808f99b375e220cbf1c88f1afc9be33cd3
J. Tran <j.yn.tran@gmail.com>
2015-05-03 18:31:43 -0400

    edited Selenium server script

--- scripts/seleniumstart.sh
+++ scripts/seleniumstart.sh
@@ -2,4 +2,4 @@
 
 # Starts the Selenium server with the appropriate Chromedriver
 
-DISPLAY=:1 xvfb-run java -jar ./node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar -Dwebdriver.chrome.driver=./node_modules/protractor/selenium/chromedriver
+DISPLAY=:1 xvfb-run -a java -jar node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar -Dwebdriver.chrome.driver=node_modules/protractor/selenium/chromedriver
