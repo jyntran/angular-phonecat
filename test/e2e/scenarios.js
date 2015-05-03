cf8fbae24f008a472620b69ff6d1e69112306953
J. Tran <j.yn.tran@gmail.com>
2015-05-03 17:01:30 -0400

    step-2 done

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,8 +12,6 @@ describe('my app', function() {
 });
 */
 
-
-
 describe('PhoneCat App', function() {
 
   describe('Phone list view', function() {
@@ -22,12 +20,10 @@ describe('PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
 
     it('should filter the phone list as a user types into the search box', function() {
-
-      var phoneList = element.all(by.repeater('phone in phones'));
-      var query = element(by.model('query'));
-
       expect(phoneList.count()).toBe(3);
 
       query.sendKeys('nexus');
@@ -37,5 +33,17 @@ describe('PhoneCat App', function() {
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
     });
+
+    it('should display the current filter value in the title bar', function() {
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    });
   });
 });
+
+
+
+
