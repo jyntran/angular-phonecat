e17681b5ad60fb69f639156867041274b284e01f
J. Tran <j.yn.tran@gmail.com>
2015-05-03 21:42:19 -0400

    revert

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,22 +2,6 @@
 
 /* Controllers */
 
-<<<<<<< HEAD
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-      // Preprocess so 5 phones appear
-      //$scope.phones = data.splice(0,5);
-    });
-
-  $scope.name = "World";
-  $scope.orderProp = 'age';
-
-}]);
-=======
 var phonecatControllers = angular.module('phonecatControllers', []);
 
 phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
@@ -33,4 +17,3 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.phoneId = $routeParams.phoneId;
   }]);
->>>>>>> tutorial-step-7
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,12 +2,7 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-<<<<<<< HEAD
-/*
-describe('my app', function() {
-=======
 describe('PhoneCat App', function() {
->>>>>>> tutorial-step-7
 
   it('should redirect index.html to index.html#/phones', function() {
     browser.get('app/index.html');
@@ -90,77 +85,3 @@ describe('PhoneCat App', function() {
     });
   });
 });
-*/
-
-describe('PhoneCat App', function() {
-
-  describe('Phone list view', function() {
-
-    beforeEach(function() {
-      browser.get('app/index.html');
-    });
-
-    var phoneList = element.all(by.repeater('phone in phones'));
-    var query = element(by.model('query'));
-
-    it('should filter the phone list as a user types into the search box', function() {
-      expect(phoneList.count()).toBe(20);
-
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(8);
-    });
-
-    it('should display the current filter value in the title bar', function() {
-      query.clear();
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
-
-      query.sendKeys('nexus');
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
-    });
-
-    it('should be possible to control phone order via the drop down select box', function() {
-
-      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
-      var query = element(by.model('query'));
-
-      function getNames() {
-        return phoneNameColumn.map(function(elm) {
-          return elm.getText();
-        });
-      }
-
-      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
-
-      expect(getNames()).toEqual([
-        "Motorola XOOM\u2122 with Wi-Fi",
-        "MOTOROLA XOOM\u2122"
-      ]);
-
-      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-      expect(getNames()).toEqual([
-        "MOTOROLA XOOM\u2122",
-        "Motorola XOOM\u2122 with Wi-Fi"
-      ]);
-    });
-
-
-
-    it('should render phone specific links', function() {
-      var query = element(by.model('query'));
-      query.sendKeys('nexus');
-      element(by.css('.phones li a')).click();
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones/nexus-s');
-      });
-    });
-  });
-});
-
-
-
-
