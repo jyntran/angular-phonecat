c800ad4ed250d87f6accc2d77d67c991809aa365
J. Tran <j.yn.tran@gmail.com>
2015-05-03 21:57:21 -0400

    step-8 done

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,72 +1,56 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-
-/*
-
-describe('controllers', function() {
-
-  it("should do something", function() {
-
-  });
-
-});
-
-*/
-
 describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function(){
-  var scope, ctrl, $httpBackend;
-
-  // Load our app module definition before each test.
   beforeEach(module('phonecatApp'));
 
-  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-  // This allows us to inject a service but then attach it to a variable
-  // with the same name as the service in order to avoid a name conflict.
-  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('phones/phones.json').
-        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl, $httpBackend;
 
-    scope = $rootScope.$new();
-    ctrl = $controller('PhoneListCtrl', {$scope: scope});
-  }));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-  it('should create "phones" model with 2 phones fetched from xhr', function() {
-    expect(scope.phones).toBeUndefined();
-    $httpBackend.flush();
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
+    }));
 
-    expect(scope.phones).toEqual([{name: 'Nexus S'},
-                               {name: 'Motorola DROID'}]);
+
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
   });
-});
-});
 
-describe('PhoneCat controllers', function() {
 
-describe('PhoneListCtrl', function(){
-  var scope, ctrl, $httpBackend;
+  describe('PhoneDetailCtrl', function(){
+    var scope, $httpBackend, ctrl;
 
-  // Load our app module definition before each test.
-  beforeEach(module('phonecatApp'));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
 
-  // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-  // This allows us to inject a service but then attach it to a variable
-  // with the same name as the service in order to avoid a name conflict.
-  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('phones/phones.json').
-        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+      $routeParams.phoneId = 'xyz';
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+    }));
 
-    scope = $rootScope.$new();
-    ctrl = $controller('PhoneListCtrl', {$scope: scope});
-  }));
 
-  it('should set the default value of orderProp model', function() {
-    expect(scope.orderProp).toBe('age');
+    it('should fetch phone detail', function() {
+      expect(scope.phone).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phone).toEqual({name:'phone xyz'});
+    });
   });
-});
-
 });
